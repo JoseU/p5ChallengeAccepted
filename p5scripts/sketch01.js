40454bf7a330c5dfdf0db2e4426c656fe8fdbc52
Cotton Mouth <cottonmouth@MacBook-Jose.local>
2017-07-29 00:46:20 -0500

    funcionando p5

--- p5scripts/sketch01.js
+++ p5scripts/sketch01.js
@@ -1,12 +1,24 @@
-var yPos = 0;
-function setup() {  // setup() runs once
-  frameRate(30);
+// primer sketch
+function setup() {
+  var canvas = createCanvas(400, 600);
+  canvas.parent('sketchHolder01');
+
+  background(51);
+  noStroke();
+
 }
-function draw() {  // draw() loops forever, until stopped
-  background(204);
-  yPos = yPos - 1;
-  if (yPos < 0) {
-    yPos = height;
+
+function draw() {
+  drawTarget(width*0.25, height*0.4, 200, 4);
+  drawTarget(width*0.5, height*0.5, 300, 10);
+  drawTarget(width*0.75, height*0.3, 120, 6);
+}
+
+function drawTarget(xloc, yloc, size, num) {
+  var grayvalues = 255/num;
+  var steps = size/num;
+  for (var i = 0; i < num; i++) {
+    fill(i*grayvalues);
+    ellipse(xloc, yloc, size - i*steps, size - i*steps);
   }
-  line(0, yPos, width, yPos);
 }
