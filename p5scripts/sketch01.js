778b3bc3f66427fbc1e0483360300015e273ccb1
Medialab <medialab@iMac-de-Medialab.local>
2017-07-28 19:31:56 -0500

    ver si funciona src

--- p5scripts/sketch01.js
+++ p5scripts/sketch01.js
@@ -9,16 +9,8 @@ function setup() {
 }
 
 function draw() {
-  drawTarget(width*0.25, height*0.4, 200, 4);
-  drawTarget(width*0.5, height*0.5, 300, 10);
-  drawTarget(width*0.75, height*0.3, 120, 6);
-}
 
-function drawTarget(xloc, yloc, size, num) {
-  var grayvalues = 255/num;
-  var steps = size/num;
-  for (var i = 0; i < num; i++) {
-    fill(i*grayvalues);
-    ellipse(xloc, yloc, size - i*steps, size - i*steps);
-  }
+  fill(23);
+  rect(random(29), 0, 10, 400);
+
 }
