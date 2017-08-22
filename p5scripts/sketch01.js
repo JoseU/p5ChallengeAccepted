975d1e4db2038d044da25f96aa789f6a0f5505fd
Cotton Mouth <cottonmouth@MacBook-Jose.local>
2017-08-22 11:41:36 -0500

    page update

--- p5scripts/sketch01.js
+++ p5scripts/sketch01.js
@@ -8,6 +8,7 @@
 // color: #9f4848; rojo
 // color: #31B0BD; turquesa 2
 // color: #3b3b3b; gris
+  var alfa =100;
 
 function setup() {
   var canvas = createCanvas(550, 550);
@@ -17,9 +18,9 @@ function setup() {
 
 function draw() {
 
-  var alfa = map(mouseX, 0, width, 70, 200);
-  var colours = [color(49, 176, 189, alfa), color(68, 165, 237, alfa), color(36, 129, 138, alfa)];
+  var offsetWeight = 0;
 
+  var colours = [color(49, 176, 189, alfa), color(68, 165, 237, alfa), color(36, 129, 138, alfa)];
 
 
   stroke(49);
@@ -28,21 +29,32 @@ function draw() {
   background(color("#e8e8e8"));
   translate(width / 2, height / 2);
 
-  var circleResolution = map(mouseY, 0, height, 2, 80);
-  var radius = mouseX - width / 2 + 0.5;
+  var circleResolution = map(mouseY, 0, height, 80, 2);
+  var radius = mouseX - width / 2;
   var angle = TWO_PI / circleResolution;
 
 
-  strokeWeight(mouseX / 30);
-
+strokeCap(PROJECT);
 
   beginShape();
+
+
+
   for (var i = 0; i < circleResolution; i++) {
+    alfa = map(mouseY, 0, height, 40, 220)+offsetWeight;
+
+
     stroke(colours[i % 3]);
     var x = cos(angle * i) * radius;
     var y = sin(angle * i) * radius;
+    offsetWeight = pow(i, 0.81803398874988);
+
+    var offsetWeight2 = (offsetWeight * 2) * -1;
+    offsetWeight2 = offsetWeight2 + offsetWeight;
+
+      strokeWeight(( radius / 30)- offsetWeight2*1.5);
     line(0, 0, x, y);
-    vertex(x, y);
+  //  vertex(x, y);
   }
   endShape(CLOSE);
 
