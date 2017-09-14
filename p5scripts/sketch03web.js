36248cf06cf6e74b31524748af6f4a41e0bffe05
Cotton Mouth <cottonmouth@MacBook-Jose.local>
2017-09-14 12:03:49 -0500

    Subida del tercer ejercicio

---
+++ p5scripts/sketch03web.js
@@ -0,0 +1,82 @@
+// Pallete colours
+
+// color: #7a7979; gris
+// color: #44A5ED; azul
+// color: #BF8ACF; morado
+// color: #56b6c2; turquesa
+// color: #D8B7AD; rosado
+// color: #9f4848; rojo
+// color: #31B0BD; turquesa 2
+// color: #3b3b3b; gris
+
+var tileCountX = 4;
+var tileCountY = 4;
+
+var count = 0;
+var drawMode = 3;
+
+function setup() {
+    var canvas = createCanvas(550, 550);
+    canvas.parent('sketchHolder03');
+    smooth();
+
+}
+
+function draw() {
+
+  var alfa = map(mouseY, 0, height, 255, 70);
+  var colours = [color(68, 165, 237, alfa), color(86, 182, 194, alfa), color(36, 129, 138, alfa), color(191, 138, 207, alfa)];
+
+    rectMode(CENTER);
+    smooth();
+    //noStroke();
+
+    noFill();
+    background(255);
+    // stroke(22);
+    // fill(100,200);
+    // rect(20,20,50,50);
+
+    count = mouseX / 20 + 5;
+    var para = mouseY / height - 0.5;
+
+
+    for (var gridY = 0; gridY <= tileCountY; gridY++) {
+
+        for (var gridX = 0; gridX <= tileCountX; gridX++) {
+
+
+
+            var tileWidth = width / tileCountX;
+            var tileHeight = height / tileCountY;
+            var posX = tileWidth * gridX + tileWidth / 2;
+            var posY = tileHeight * gridY + tileHeight / 2;
+
+
+            push();
+            translate(posX, posY);
+
+            //switch between modules
+
+            switch (drawMode) {
+
+                case 3:
+                    for (var i = 0; i <= count; i++) {
+                      line(0, para*tileHeight, tileWidth/2, (i/count-0.5)*tileHeight);
+                        line(0, para*tileHeight, -tileWidth/2, (i/count-0.5)*tileHeight);
+                        line(0, para*tileHeight, (i/count-0.5)*tileWidth, tileHeight/2);
+                        line(0, para*tileHeight, (i/count-0.5)*tileWidth, -tileHeight/2);
+                    }
+                    break;
+            }
+
+
+  pop();
+
+        }
+
+
+    }
+
+
+}
